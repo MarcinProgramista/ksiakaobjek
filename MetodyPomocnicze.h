f1990171d7b35fb2a27fdbe1d4337166947f72bb
Marcin Czapla <marcin.czapla.programista@gmail.com>
2019-06-04 08:07:10 +0200

    Added menu to file main

--- MetodyPomocnicze.h
+++ MetodyPomocnicze.h
@@ -15,5 +15,6 @@ public:
     static string zamienPierwszaLitereNaDuzaAPozostaleNaMale(string tekst);
     static int konwersjaStringNaInt(string liczba);
     static string pobierzLiczbe(string tekst, int pozycjaZnaku);
+    static char wczytajZnak();
 };
 #endif
